6221154a6fba975e94c4f3b26e92f0f9bd938d1a
jlchereau <jlchereau@msn.com>
2022-06-03 09:05:51 +0100

    Commented loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
+        // The loader can be found at ./lib/loader.js
+        // It is configured on each <Image/> component (next/image)
+        // Cf. https://nextjs.org/docs/api-reference/next/image#loader
         loader: 'custom',
     },
 }
